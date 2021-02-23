0094c2e8de133b8e2561d4209af651b55ad72744
dboyer <dboyer@student.42.fr>
2021-02-23 16:21:02 +0100

    need test on guacamole

--- philo_two/srcs/ft_clean_table.c
+++ philo_two/srcs/ft_clean_table.c
@@ -6,7 +6,7 @@
 /*   By: dboyer <dboyer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/19 15:37:06 by dboyer            #+#    #+#             */
-/*   Updated: 2021/02/21 15:34:39 by dboyer           ###   ########.fr       */
+/*   Updated: 2021/02/23 16:19:10 by dboyer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 void	ft_clean_table(t_table *table)
 {
-	sem_unlink(SEM_FORKS);
-	sem_unlink(SEM_OUTPUT);
 	if (table)
 	{
 		sem_close(table->lock_output);
@@ -23,4 +21,6 @@ void	ft_clean_table(t_table *table)
 		if (table->philosophers)
 			free(table->philosophers);
 	}
+	sem_unlink(SEM_FORKS);
+	sem_unlink(SEM_OUTPUT);
 }
